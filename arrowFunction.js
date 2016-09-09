c3749bb68cd0bf920a93b3510502d586a0c8368a
vogtn <nicolasjvogt@gmail.com>
2016-09-09 14:33:58 -0700

    function vs. arrowfunction

---
+++ arrowFunction.js
@@ -0,0 +1,10 @@
+//standard function
+var createGreeting = function(message, name){
+	return message + name;
+}
+
+//arrow function
+var arrowGreeting = (message, name) => {
+	return message + name;
+}
+
